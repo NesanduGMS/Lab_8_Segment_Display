6de0e7391c096335e310679d65885b320bc86742
Nesandu Sithnuka <157829526+NesanduGMS@users.noreply.github.com>
2024-03-22 21:08:55 +0530

    Add files via upload

---
+++ LUT_16_7_Sim.vhd
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/19/2024 12:51:45 PM
+-- Design Name: 
+-- Module Name: LUT_16_7_Sim - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity LUT_16_7_Sim is
+--  Port ( );
+end LUT_16_7_Sim;
+
+architecture Behavioral of LUT_16_7_Sim is
+
+component LUT_16_7
+    port(address : in STD_LOGIC_VECTOR (3 downto 0);
+        data : out STD_LOGIC_VECTOR (6 downto 0)
+        );
+end component;
+
+signal address : STD_LOGIC_VECTOR (3 downto 0);
+signal data : STD_LOGIC_VECTOR (6 downto 0);
+
+begin
+
+    UUT: LUT_16_7 port map(
+        address => address,
+        data => data
+    );
+
+    process begin
+        address <= "1001";
+        WAIT FOR 100ns;
+        address <= "1000";
+        WAIT FOR 100ns;
+        address <= "1101";
+        WAIT FOR 100ns;
+        address <= "0101";
+        WAIT FOR 100ns;
+        
+        wait;
+    end process;
+
+end Behavioral;
